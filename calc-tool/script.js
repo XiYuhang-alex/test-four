9a689f13eb1435e85ff7edff89e5510ccf057755
XiYuhang-alex <1740151061@qq.com>
2026-09-11 16:19:12 +0800

    修正消费数据，混入非法数据

--- calc-tool/script.js
+++ calc-tool/script.js
@@ -3,5 +3,6 @@ const records = [
 	{ name: '坐公交车', amount: 1},
 	{ name: '午餐', amount: 15},
 	{ name: '晚餐', amount: 12},
-	{ name: '小零食', amount: 5}
+	{ name: '小零食', amount: 5},
+	{ name: '奶茶', amount: -20}
 ];
